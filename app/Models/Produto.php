b5386965816b1e95e6a63589ce861daee3759f90
Leandro <leandrodev83@gmail.com>
2024-02-15 14:00:31 -0300

    feat: cria relacinamentos entre tabelas categoria e produto

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Produto extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'categoria_id',
+        'codigo_barra',
+        'nome_produto',
+        'marca',
+        'valor_custo',
+        'valor_margem',
+        'valor_venda',
+        'estoque',
+        'estoque_minimo'
+    ];
+
+    public function categoria() {
+        return $this->belongsTo(Categoria::class);
+    }
 }
--- database/migrations/2024_02_15_164839_create_produtos_table.php
+++ database/migrations/2024_02_15_164839_create_produtos_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('produtos', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('categoria_id');
+            $table->foreign('categoria_id')->references('id')->on('categorias')->onDelete('cascade')->onUpdate('cascade');
+            $table->string('codigo_intero');
+            $table->string('codigo_barra');
+            $table->string('nome_produto');
+            $table->string('marca');
+            $table->double('valor_custo', 8,2);
+            $table->double('valor_margem', 8,2);
+            $table->double('valor_venda',8,2);
+            $table->integer('estoque');
+            $table->integer('estoque_minimo');
             $table->timestamps();
         });
     }
